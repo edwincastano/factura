31932f7e88067a6dfec60ea555d732ca14be7119
edwincastano <edwin_castano79@hotmail.com>
2021-03-29 22:29:18 -0500

    .

--- factura/factura/core/models.py
+++ factura/factura/core/models.py
@@ -3,9 +3,13 @@ from django.db import models
 
 class Autor(models.Model):
     id = models.AutoField(primary_key = True)
-    nombre = models.CharField(max_length = 60)
-    apellido = models.CharField(max_length = 60)
-    correo = models.EmailField(max_length = 30)
+    cliente = models.CharField(max_length = 60)
+    direccion = models.CharField(max_length = 60)
+    articulo = models.AutoField(max_length = 60)
+    cantidad = models.AutoField(max_length = 30)
+    valor unitario = models.CharField(max_length = 30)
+    valor total = models.CharField(max_length = 30)
+
 
     def __str__(self):
         return self.nombre
